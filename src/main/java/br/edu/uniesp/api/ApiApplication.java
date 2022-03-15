7863993f1c078e9a7a891ff480a1930ad449608c
Fujioka <rodrigofujioka@gmail.com>
2022-03-15 19:37:32 -0300

    Up project

---
+++ src/main/java/br/edu/uniesp/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.uniesp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
