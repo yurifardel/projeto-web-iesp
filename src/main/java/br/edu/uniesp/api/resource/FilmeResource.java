939ee2faeae3a47c2bd0bcaa246b49b7a4df8f1d
Fujioka <rodrigofujioka@gmail.com>
2022-03-22 17:50:03 -0300

    Inclusão de material da aula02

--- src/main/java/br/edu/uniesp/api/resource/FilmeResource.java
+++ src/main/java/br/edu/uniesp/api/resource/FilmeResource.java
@@ -1,20 +1,15 @@
 package br.edu.uniesp.api.resource;
 
 import br.edu.uniesp.api.model.Filme;
-import br.edu.uniesp.api.service.FilmeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/filme")
 public class FilmeResource {
 
-    @Autowired
-    private FilmeService service;
-
     @GetMapping("/teste")
     public Filme teste(){
-        Filme f = service.getFilme();
+        Filme f = new Filme();
         f.setTitulo("Jurassic Park");
         return f;
     }
